474198137a8a88cdfea790f18765cb460e500bab
gerenciamentodecontasorlando-create <gerenciamentodecontasorlando@gmail.com>
2026-02-13 07:24:52 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'dr-orlando-abreu-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/icons/icon-192x192.png'
+];
+
+// Instalar service worker e cachear arquivos
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Buscar arquivos do cache ou da internet
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Atualizar service worker
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -
